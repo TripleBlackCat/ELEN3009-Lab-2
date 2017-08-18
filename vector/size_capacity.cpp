585ebfc32aac010077593edc22415824b5aded09
TripleBlackCat <kavilann@gmail.com>
2017-08-18 22:45:19 +0200

    Exercise 2.1

--- vector/size_capacity.cpp
+++ vector/size_capacity.cpp
@@ -11,6 +11,11 @@ int main()
     vector<int> vec;
     cout << "vec: size: " << vec.size() 
          << " capacity: " << vec.capacity() << endl;
+        
+    // The size of the vector is the number of elements stored in the vector. 
+    // The capacity of the vector is the maximum number of elements before reallocation
+    // A vector is a dynamic array of elements. It has a capacity of 0 when it is first initialized and empty. As soon as the number of elements inside the array reaches the capacity, the capacity doubles to allow for more elements to be inserted.
+
 
     for(int i = 0; i < 24; i++) {
         vec.push_back(i);
